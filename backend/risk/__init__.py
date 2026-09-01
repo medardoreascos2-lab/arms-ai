8cadfc2c8a168610aa1ba2782aedbd89e63f2c16
Medardo Reascos <medardoreascos2@gmail.com>
2026-09-01 10:35:57 -0400

    refactor: remove superseded signal controller

--- backend/risk/__init__.py
+++ backend/risk/__init__.py
@@ -2,14 +2,8 @@ from backend.risk.risk_manager import (
     RiskManager,
 )
 
-from backend.risk.signal_controller_v1 import (
-    SignalControllerV1,
-    SignalControlResult,
-)
 
 
 __all__ = [
     "RiskManager",
-    "SignalControllerV1",
-    "SignalControlResult",
 ]
